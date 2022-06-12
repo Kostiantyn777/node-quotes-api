db5dc060d713a72fb35e60479d025b55f0202f20
Kostiantyn777 <borodachkostiantyn@gmail.com>
2022-06-12 18:56:41 +0200

    Changed the quote API server to allow updating a quote according to the given ID.

--- server.js
+++ server.js
@@ -15,8 +15,12 @@ app.get("/quotes", function (request, response) {
 app.get("/quotes/:id", function (request, response) {
   const requestedId = request.params.id;
 
-  const filteredByID = quotes.filter((element) => element.id == requestedId);
-
+  const filteredByID = quotes.find((element) => element.id == requestedId);
+  if (!filteredByID) {
+    return response
+      .status(404)
+      .send(`Quote with ID ${requestedId}  was not found`);
+  }
   response.send(filteredByID);
 });
 
@@ -24,6 +28,16 @@ app.post("/quotes", function (request, response) {
   console.log("POST /quotes route");
   console.log(request.body);
   quotes.push(request.body);
+});
+
+app.put("/quotes/:id", function (request, response) {
+  const quotesId = request.params.id;
+  const requestBody = request.body;
+  const { quote, author } = requestBody;
+  const result = quotes.find((element) => element.id == quotesId);
+  result.quote = quote;
+  result.author = author;
+  response.status(200).send("OK");
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
