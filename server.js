fdc0a130ebff29a00195553efd787382ac5dbd81
Kostiantyn777 <borodachkostiantyn@gmail.com>
2022-06-12 17:56:56 +0200

    Changed a quote API server to allow POSTs of new quotes.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
 const quotes = require("./quotes.json");
 
 app.get("/", function (request, response) {
@@ -17,6 +18,12 @@ app.get("/quotes/:id", function (request, response) {
   const filteredByID = quotes.filter((element) => element.id == requestedId);
 
   response.send(filteredByID);
+});
+
+app.post("/quotes", function (request, response) {
+  console.log("POST /quotes route");
+  console.log(request.body);
+  quotes.push(request.body);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
