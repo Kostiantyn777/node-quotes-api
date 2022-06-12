f647bf42ce8849da7228d04f3e7c7b919da16db7
Kostiantyn777 <borodachkostiantyn@gmail.com>
2022-06-12 17:05:30 +0200

    Changed a quote API server to allow GETting a quote according to the given ID.

--- server.js
+++ server.js
@@ -3,12 +3,20 @@ const app = express();
 
 const quotes = require("./quotes.json");
 
-app.get('/', function(request, response) {
-  response.send('/quotes/17 should return one quote, by id')
+app.get("/", function (request, response) {
+  response.send("/quotes/17 should return one quote, by id");
 });
 
-app.get("/quotes", function(request, response){
-  response.json(quotes);
+app.get("/quotes", function (request, response) {
+  response.send(quotes);
+});
+
+app.get("/quotes/:id", function (request, response) {
+  const requestedId = request.params.id;
+
+  const filteredByID = quotes.filter((element) => element.id == requestedId);
+
+  response.send(filteredByID);
 });
 
 app.listen(3000, () => console.log("Listening on port 3000"));
